a36b78dd6c28ad1f4f36d5222313dee2ff826031
isabarrenha <barrenha95@yahoo.com.br>
2024-10-21 09:40:26 -0300

    json-input-validation-block

--- modules/json_module.py
+++ modules/json_module.py
@@ -29,12 +29,51 @@ class JsonOperations:
         
             json_object = json.dumps(dictionary, indent=4)
         
-            with open('./database/users.json', "w") as outfile:
+            with open(self.path, "w") as outfile:
                 outfile.write(json_object)
 
+    def input_validation(self, first, last, id5):
 
+        try:
+            str(first)
+        except: 
+            raise Exception("First name must string type")
+        
+        try:
+            str(last)
+        except: 
+            raise Exception("Last name must string type")
+
+        try:
+            int(id5)
+        except: 
+            raise Exception("The last five id numbers must be integer type")
+
+        if len(first) > 10:
+            raise Exception("Enter a first name with less than 10 characters")                
+
+        if len(last) > 10:
+            raise Exception("Enter a last name with less than 10 characters")                        
+        
+        if len(str(id5)) > 5:
+            raise Exception("You entered more than 5 digits in the last five id")
+        
+        if len(str(id5)) < 5:
+            raise Exception("You entered less than 5 digits in the last five id")
+        
+        if len(first) == 0:
+            raise Exception("First name is blank.")
+        
+        if len(last) == 0:
+            raise Exception("Last name is blank.")
+        
+        if len(str(id5)) == 0:
+            raise Exception("Last five id is blank.")
+
+    #def add_user(self, first, last, id5):
+        
+        
 
 if __name__ == '__main__':
-    
     jsonoperations = JsonOperations()
-    
+    jsonoperations.input_validation(first = 'teste', last = 'teste2', id5 = 12345)
