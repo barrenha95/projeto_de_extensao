90c7f16dc29f31c5f28c09389511799d109d7971
isabarrenha <barrenha95@yahoo.com.br>
2024-10-22 09:47:29 -0300

    removing-unused-parts

--- modules/json_module.py
+++ modules/json_module.py
@@ -98,17 +98,6 @@ class JsonOperations:
         if finder == match:
             #print(1)
             return 1
-    
-    def merge(a: dict, b: dict, path=[]):
-        for key in b:
-            if key in a:
-                if isinstance(a[key], dict) and isinstance(b[key], dict):
-                    merge(a[key], b[key], path + [str(key)])
-                elif a[key] != b[key]:
-                    raise Exception('Conflict at ' + '.'.join(path + [str(key)]))
-            else:
-                a[key] = b[key]
-        return a
 
     def add_user(self, first, last, id5):
         """
