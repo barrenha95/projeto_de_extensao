cfc716f7bf7409e1bbaa9a50bf35a8128719cb27
isabarrenha <barrenha95@yahoo.com.br>
2024-10-23 09:38:12 -0300

    form-from-scratch

--- app.py
+++ app.py
@@ -2,20 +2,24 @@ from flask import Flask, render_template#, jsonify
 
 app = Flask(__name__)
 
-messages = [{'title': 'Message One',
-             'content': 'Message One Content'},
-            {'title': 'Message Two',
-             'content': 'Message Two Content'}
+messages = [{'title': 'Register',
+             'content': 'Add someone on the event allowed list.'},
+            {'title': 'Remove',
+             'content': 'Remove someonte on the event allowed list.'}
             ]
 
 @app.route('/')
 def index():
     return render_template('/index.html', messages=messages)
 
-#@app.route('/auth/<firstname>/<lastname>/<last5cpf>', methods=['GET'])
-##example: http://127.0.0.1:5000/auth/average/guy/88745
-#def api(firstname, lastname, last5cpf):
-#    return f'param1: {firstname}, param2: {lastname}, param3: {last5cpf}'
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    return render_template('create.html')
+
+@app.route('/auth/<firstname>/<lastname>/<last5cpf>', methods=['GET'])
+#example: http://127.0.0.1:5000/auth/average/guy/88745
+def api(firstname, lastname, last5cpf):
+    return f'param1: {firstname}, param2: {lastname}, param3: {last5cpf}'
 
 
 #app.run()
